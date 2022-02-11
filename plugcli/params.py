e4c7a7109131100923f41cb5adfaaf5e3771403c
David W.H. Swenson <david.swenson@omsf.io>
2022-02-11 16:10:29 -0500

    Initial version based on OPS CLI

---
+++ plugcli/params.py
@@ -0,0 +1,110 @@
+import click
+
+
+def _decorator_not_implemented(*args, **kwargs):
+    raise NotImplementedError("'decorator' is not implemented for this "
+                              "class")
+
+
+class AbstractParameter:
+    """
+    Abstract wrapper for click parameters.
+
+    Parameters
+    ----------
+    args :
+        Arguments to pass to click parameter
+    getter : Callable
+        function to create desired object from user input and context dict;
+        default behavior is to return user input; keyword-only
+    kwargs :
+        Keyword arguments to pass to click parameter
+    """
+    decorator = _decorator_not_implemented
+    def __init__(self, *args, getter=None, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        if getter is None:
+            getter = lambda user_input, context: user_input
+        self.getter = getter
+
+    def parameter(self, **kwargs):
+        """Return the click decorator for this parameter
+
+        Parameters
+        ----------
+        kwargs :
+            additional click parameters; overrides anything set in
+            initialization
+
+        Returns
+        Callable :
+            the click decorator to use in a command
+        """
+        # kwargs given here override those given on initialization
+        decorator_kwargs = dict(**self.kwargs, **kwargs)
+        dec = self.__class__.decorator(*self.args, **decorator_kwargs)
+        return dec
+
+    def get(self, user_input, context=None):
+        """Convert user input to library object using provided getter
+
+        Parameters
+        ----------
+        user_input :
+            input as handled by click decorators
+        context : Dict[str, Any]
+            dict mapping labels to other objects that may be used by this.
+        """
+        return self.getter(user_input, context)
+
+
+class Option(AbstractParameter):
+    """Wrapper for click.option decorators"""
+    decorator = click.option
+
+
+class Argument(AbstractParameter):
+    """Wrapper for click.argument decorators"""
+    decorator = click.argument
+
+
+NOT_PARSED = object()
+
+
+class MultiStrategyGetter:
+    """
+    Callable that combines attempts multiple strategies to parse user input.
+
+    Parameters
+    ----------
+    strategies : List[Callable[str, Dict[str, Any], Any]]
+        list of callables, each representing one strategy for creating the
+        desired object from the user input.
+    error_message : str
+        message to provide if no object found; ``.format`` will be used to
+        expand ``user_input``
+    """
+    def __init__(self, strategies, error_message):
+        self.strategies = strategies
+        self.error_message = error_message
+
+    def __call__(self, user_input, context=None):
+        """
+        Parameters
+        ----------
+        user_input :
+            parameter as handled by click decorators
+        context : Dict
+            mapping of str to additional information available to help
+            create this object
+        """
+        if context is None:
+            context = {}
+
+        for strategy in self.strategies:
+            found = strategy(user_input, context)
+            if found is not NOT_PARSED:
+                return found
+
+        raise click.BadInput(error_message.format(user_input=user_input))
---
+++ plugcli/plugin_management.py
@@ -0,0 +1,197 @@
+import collections
+import pkgutil
+import importlib
+import warnings
+import os
+
+class PluginRegistrationError(RuntimeError):
+    pass
+
+class Plugin(object):
+    """Generic plugin object
+
+    parameters
+    ----------
+    requires_lib: tuple
+        tuple representing the minimum allowed version of the underlying
+        library
+    requires_cli: tuple
+        tuple representing hte minimum allowed version of the command line
+        interface application
+    """
+    def __init__(self, requires_lib, requires_cli):
+        self.requires_lib = requires_lib
+        self.requires_cli = requires_cli
+        self.location = None
+        self.plugin_type = None
+
+    def attach_metadata(self, location, plugin_type):
+        # error is already registered and data doesn't match
+        error_condition = (
+            (self.location is not None or self.plugin_type is not None)
+            and (self.location != location
+                 or self.plugin_type != plugin_type)
+        )
+        if error_condition:  # -no-cov-
+            msg = (
+                f"The plugin {repr(self)} has been previously "
+                "registered with different metadata."
+            )
+            raise PluginRegistrationError(msg)
+
+        self.location = location
+        self.plugin_type = plugin_type
+
+
+class CommandPlugin(Plugin):
+    """Mix-in class for command plugins.
+    """
+    def __init__(self, command, section, **kwargs):
+        self.command = command
+        self.section = section
+        super().__init__(**kwargs)
+
+    @property
+    def name(self):
+        return self.command.name
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.name})"
+
+
+class CLIPluginLoader(object):
+    """Abstract object for CLI plugins
+
+    The overall approach involves 3 steps:
+
+    1. Find modules in the relevant locations
+    2. Load the namespaces associated into a dict (nsdict)
+    3. Find all objects in those namespaces that are plugins
+
+    Additionally, we attach metadata about where the plugin was found and
+    what mechamism it was loaded with.
+
+    Details on steps 1 and 2 differ based on whether this is a
+    filesystem-based plugin or a namespace-based plugin, and details on step
+    3 can depend on the specific instance created. By default, it looks for
+    instances of :class:`.Plugin` (given as ``plugin_class``) but the
+    ``isinstance`` check can be overridden in subclasses.
+
+    Parameters
+    ----------
+    plugin_type : Literal["file", "namespace"]
+        the type of file
+    search_path : str
+        the directory or namespace to search for plugins
+    plugin_class: type
+        plugins are identified as instances of this class (override in
+        ``_is_my_plugin``)
+    """
+    def __init__(self, plugin_type, search_path, plugin_class=Plugin):
+        self.plugin_type = plugin_type
+        self.search_path = search_path
+        self.plugin_class = plugin_class
+
+    # TODO: this should be _find_candidate_modules
+    def _find_candidates(self):
+        raise NotImplementedError()
+
+    @staticmethod
+    def _make_nsdict(candidate):
+        raise NotImplementedError()
+
+    def _find_candidate_namespaces(self):
+        candidates = self._find_candidates()
+        namespaces = {cand: self._make_nsdict(cand) for cand in candidates}
+        return namespaces
+
+    def _is_my_plugin(self, obj):
+        return isinstance(obj, self.plugin_class)
+
+    def _find_plugins(self, namespaces):
+        for loc, ns in namespaces.items():
+            for obj in ns.values():
+                if self._is_my_plugin(obj):
+                    obj.attach_metadata(loc, self.plugin_type)
+                    yield obj
+
+    def __call__(self):
+        namespaces = self._find_candidate_namespaces()
+        plugins = list(self._find_plugins(namespaces))
+        return plugins
+
+
+class FilePluginLoader(CLIPluginLoader):
+    """File-based plugins (quick and dirty)
+
+    Parameters
+    ----------
+    search_path : str
+        path to the directory that contains plugins (OS-dependent format)
+    plugin_class: type
+        plugins are identified as instances of this class (override in
+        ``_is_my_plugin``)
+    """
+    def __init__(self, search_path, plugin_class):
+        super().__init__(plugin_type="file", search_path=search_path,
+                         plugin_class=plugin_class)
+
+    def _find_candidates(self):
+        def is_plugin(filename):
+            return (
+                filename.endswith(".py") and not filename.startswith("_")
+                and not filename.startswith(".")
+            )
+
+        if not os.path.exists(os.path.join(self.search_path)):
+            return []
+
+        candidates = [os.path.join(self.search_path, f)
+                      for f in os.listdir(self.search_path)
+                      if is_plugin(f)]
+        return candidates
+
+    @staticmethod
+    def _make_nsdict(candidate):
+        ns = {}
+        with open(candidate) as f:
+            code = compile(f.read(), candidate, 'exec')
+            eval(code, ns, ns)
+        return ns
+
+
+class NamespacePluginLoader(CLIPluginLoader):
+    """Load namespace plugins (plugins for wide distribution)
+
+    Parameters
+    ----------
+    search_path : str
+        namespace (dot-separated) where plugins can be found
+    plugin_class: type
+        plugins are identified as instances of this class (override in
+        ``_is_my_plugin``)
+    """
+    def __init__(self, search_path, plugin_class):
+        super().__init__(plugin_type="namespace", search_path=search_path,
+                         plugin_class=plugin_class)
+
+    def _find_candidates(self):
+        # based on https://packaging.python.org/guides/creating-and-discovering-plugins/#using-namespace-packages
+        def iter_namespace(ns_pkg):
+            return pkgutil.iter_modules(ns_pkg.__path__,
+                                        ns_pkg.__name__ + ".")
+
+        try:
+            ns = importlib.import_module(self.search_path)
+        except ModuleNotFoundError:
+            candidates = []
+        else:
+            candidates = [
+                importlib.import_module(name)
+                for _, name, _ in iter_namespace(ns)
+            ]
+        return candidates
+
+    @staticmethod
+    def _make_nsdict(candidate):
+        return vars(candidate)
